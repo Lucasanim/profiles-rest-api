9e94ab188117999b5a1599025038c3cfb7d38c06
lucas <lucasanim12@gmail.com>
2020-12-22 16:59:58 -0300

    added api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class Hello(APIView):
+    """Test"""
+    def get(self, request, format=None):
+        an_apiview = [
+            'use http methods'
+        ]
+
+        return Response({'message':'hello', 'an_apiview':an_apiview})
